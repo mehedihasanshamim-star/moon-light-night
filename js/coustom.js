dd4cd554417df84b34e4e993f4be7e30cac92a33
mehedihasanshamim-star <57722840+mehedihasanshamim-star@users.noreply.github.com>
2020-07-03 19:11:39 +0600

    Add files via upload

---
+++ js/coustom.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	
+
+	/* -- out- typed js --*/
+	var typed = new Typed('.typing-1', {
+		strings: ["A full moon:",
+		"I love a night", 
+		"that simple",
+		"Good night",
+		],
+		loop:true,
+		backSpeed:100,
+		typeSpeed: 100,
+		backDelay: 900
+	});
+
+});
